84dfb5f8173662f16bdfb3a534bf89be83db44ba
Daniel Agoston <dann.agoston@gmail.com>
2024-06-05 11:53:10 +0100

    form_pages HTML and backend done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def form_page
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'form_page', to: 'pages#form_page'
 end
