486b71579bb630781f4a0ad250bbf821a06ade7c
Jordan den Dulk <jordan.dirk.dendulk@gmail.com>
2024-06-10 18:01:32 +0100

    count-down-timer-monday-afternoon

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -1,39 +1,104 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  countdown = null;
+
+  static values = {
+    duration: Number
+  }
+
   connect () {
+
+    this.hour = Math.floor(this.durationValue / 60)
+    this.minutes = (this.durationValue - this.hour * 60)
+    if (this.minutes == 0) {
+      this.hour -= 1
+      this.minutes = 59
+    } else {
+      this.minutes -= 1
+    }
+
+    this.seconds = 59
+    // console.log(this.hour);
+    // console.log(this.minutes);
     this.startCountdown();
   }
 
-    updateTimer() {
-      const currentTime = Math.floor(Date.now() / 1000);
-      const remainingTime = this.endTimeValue - currentTime;
-      const minutes = Math.floor(remainingTime / 60);
-      const seconds = remainingTime % 60;
+    // updateTimer() {
+    //   const currentTime = Math.floor(Date.now() / 1000);
+    //   const remainingTime = this.endTimeValue - currentTime;
+    //   const minutes = Math.floor(remainingTime / 60);
+    //   const seconds = remainingTime % 60;
 
-      this.timerTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-    }
+    //   this.timerTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+    // }
     startCountdown() {
-      const timerTarget = this.targets.find("timer");
-      const duration = parseInt(this.data.get("duration"));
-      const endTime = new Date().getTime() + (duration * 60000);
+      setInterval(() => {
 
-      const updateTimer =() => {
-        const now = new Date().getTime();
-        const distance = endTimer - now;
-          if (distance < 0) {
-            clearInterval(countdown);
-            timerTarget.textContent = "EXPIRED";
-          } else {
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            timerTarget.textContent = `${hours}h ${minutes}m ${seconds}s`;
+
+      if (this.minutes == 0 ) {
+            this.hour -= 1
+            this.seconds = 59
           }
-        };
+          if ((this.hour > 0 && this.minutes >= 0) && (this.seconds >= 0 )) {
+            this.seconds -= 1
+          }
 
-      updateTimer();
-      const countdown = setInterval(updateTimer, 1000);
+
+          if (this.seconds == 0 ) {
+            this.seconds = 59
+            this.minutes -= 1
+            if (this.minutes == 0 && this.hour >= 1) {
+                this.hour -= 1
+                this.minutes = 59
+              } else if (this.minutes == 0 && this.hour < 1) {
+                this.minutes = 0
+                this.hours = 0
+                this.seconds = 0
+                this.element.innerText = `THIS MEETING HAS EXPIRED ${this.hour}:${this.minutes}:${this.seconds}`;
+              }
+            }
+            this.element.innerText = `${this.hour}:${this.minutes}:${this.seconds}`
+            // if (this.hour == 0 && this.minutes == 0 && this.seconds == 0 ) {
+            //   this.element.innerText = "EXPIRED"; }
+          }, 1);
+
+
+
+
+          // } else if (this.hour == 0 && this.minutes == 0 && this.seconds > 0 ) {
+          //   this.seconds -= 1;
+          // } else if (this.hour == 0 && this.minutes > 0 ) {
+          //   this.seconds -= 1
+          // }
+
+
+
+
+
+
+
+      // const timerTarget = this.targets.find("timer");
+      // const duration = parseInt(this.data.get("duration"));
+      // const endTime = new Date().getTime() + (duration * 60000);
+
+      // const updateTimer =() => {
+      //   const now = new Date().getTime();
+      //   const distance = endTime - now;
+      //     if (distance < 0) {
+      //       clearInterval(countdown);
+      //       this.timerTarget.textContent = "EXPIRED";
+      //     } else {
+      //       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      //       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      //       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      //       this.timerTarget.textContent = `${hours}h ${minutes}m ${seconds}s`;
+      //     }
+      //   };
+
+      // updateTimer();
+      // const countdown = setInterval(updateTimer, 1000);
     }
   }
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -10,15 +10,20 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
-    setTimeout(() => {
-      this.map = new mapboxgl.Map({
-        container: this.element,
-        style: "mapbox://styles/mapbox/streets-v10"
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
       })
-      this.#addMarkersToMap()
-      this.#fitMapToMarkers()
+      this.#addCurrentLocationToMap()
+        if (this.markersValue.length > 0) {
+          this.#addMarkersToMap()
+          this.#fitMapToMarkers()
+        }
 
-    }, 1)
+      window.setTimeout(() => {
+      document.querySelector(".mapboxgl-ctrl-geolocate").click();
+
+      }, 200);
 
   }
   #addMarkersToMap() {
@@ -32,5 +37,16 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+  #addCurrentLocationToMap() {
+    const geoLocate = new mapboxgl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true,
+      showUserHeading: true
+    })
+    this.map.addControl(geoLocate)
+    // console.log(geoLocate)
   }
 }
