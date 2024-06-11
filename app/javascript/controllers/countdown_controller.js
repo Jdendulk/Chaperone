52cf01d27ef3680fcdc00b52a83d2ef86176bef0
Jordan den Dulk <jordan.dirk.dendulk@gmail.com>
2024-06-11 11:02:02 +0100

    count-down-timer-logic-complete

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -7,8 +7,7 @@ export default class extends Controller {
     duration: Number
   }
 
-  connect () {
-
+  connect() {
     this.hour = Math.floor(this.durationValue / 60)
     this.minutes = (this.durationValue - this.hour * 60)
     if (this.minutes == 0) {
@@ -24,81 +23,94 @@ export default class extends Controller {
     this.startCountdown();
   }
 
-    // updateTimer() {
-    //   const currentTime = Math.floor(Date.now() / 1000);
-    //   const remainingTime = this.endTimeValue - currentTime;
-    //   const minutes = Math.floor(remainingTime / 60);
-    //   const seconds = remainingTime % 60;
-
-    //   this.timerTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-    // }
-    startCountdown() {
-      setInterval(() => {
-
-
-
-      if (this.minutes == 0 ) {
-            this.hour -= 1
-            this.seconds = 59
-          }
-          if ((this.hour > 0 && this.minutes >= 0) && (this.seconds >= 0 )) {
-            this.seconds -= 1
-          }
-
-
-          if (this.seconds == 0 ) {
-            this.seconds = 59
-            this.minutes -= 1
-            if (this.minutes == 0 && this.hour >= 1) {
-                this.hour -= 1
-                this.minutes = 59
-              } else if (this.minutes == 0 && this.hour < 1) {
-                this.minutes = 0
-                this.hours = 0
-                this.seconds = 0
-                this.element.innerText = `THIS MEETING HAS EXPIRED ${this.hour}:${this.minutes}:${this.seconds}`;
-              }
-            }
-            this.element.innerText = `${this.hour}:${this.minutes}:${this.seconds}`
-            // if (this.hour == 0 && this.minutes == 0 && this.seconds == 0 ) {
-            //   this.element.innerText = "EXPIRED"; }
-          }, 1);
-
-
-
-
-          // } else if (this.hour == 0 && this.minutes == 0 && this.seconds > 0 ) {
-          //   this.seconds -= 1;
-          // } else if (this.hour == 0 && this.minutes > 0 ) {
-          //   this.seconds -= 1
-          // }
-
-
-
-
-
-
-
-      // const timerTarget = this.targets.find("timer");
-      // const duration = parseInt(this.data.get("duration"));
-      // const endTime = new Date().getTime() + (duration * 60000);
-
-      // const updateTimer =() => {
-      //   const now = new Date().getTime();
-      //   const distance = endTime - now;
-      //     if (distance < 0) {
-      //       clearInterval(countdown);
-      //       this.timerTarget.textContent = "EXPIRED";
-      //     } else {
-      //       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      //       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      //       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-      //       this.timerTarget.textContent = `${hours}h ${minutes}m ${seconds}s`;
-      //     }
-      //   };
-
-      // updateTimer();
-      // const countdown = setInterval(updateTimer, 1000);
-    }
+  disconnect() {
+    clearInterval(this.countdown)
   }
+
+  // updateTimer() {
+  //   const currentTime = Math.floor(Date.now() / 1000);
+  //   const remainingTime = this.endTimeValue - currentTime;
+  //   const minutes = Math.floor(remainingTime / 60);
+  //   const seconds = remainingTime % 60;
+
+  //   this.timerTarget.textContent = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  // }
+  startCountdown() {
+    this.countdown = setInterval(() => {
+
+      console.log("hour", this.hour)
+      console.log("minutes", this.minutes)
+      console.log("seconds", this.seconds)
+
+      if (this.hour <= 0 && this.minutes <= 0 && this.seconds <= 0) {
+        this.element.innerText = `THIS MEETING HAS EXPIRED ${this.hour}:${this.minutes}:${this.seconds}`;
+        return clearInterval(this.countdown)
+      }
+
+      if (this.minutes === 0) {
+        this.hour -= 1
+        this.seconds = 59
+      }
+      if ((this.hour >= 0 && this.minutes >= 0) && (this.seconds >= 0)) {
+        this.seconds -= 1
+      }
+
+
+      if (this.seconds === 0) {
+        this.seconds = 59
+        this.minutes -= 1
+        if (this.minutes == 0 && this.hour >= 1) {
+          this.hour -= 1
+          this.minutes = 59
+        } else if (this.minutes <= 0 && this.hour < 1) {
+          this.minutes = 0
+          this.hours = 0
+          this.seconds = 0
+        }
+      }
+
+      this.element.innerText = `${this.hour}:${this.minutes}:${this.seconds}`
+      // if (this.hour == 0 && this.minutes == 0 && this.seconds == 0 ) {
+      //   this.element.innerText = "EXPIRED"; }
+    }, 1);
+
+    clearInterval()
+
+
+
+
+    // } else if (this.hour == 0 && this.minutes == 0 && this.seconds > 0 ) {
+    //   this.seconds -= 1;
+    // } else if (this.hour == 0 && this.minutes > 0 ) {
+    //   this.seconds -= 1
+    // }
+
+
+
+
+
+
+
+    // const timerTarget = this.targets.find("timer");
+    // const duration = parseInt(this.data.get("duration"));
+    // const endTime = new Date().getTime() + (duration * 60000);
+
+    // const updateTimer =() => {
+    //   const now = new Date().getTime();
+    //   const distance = endTime - now;
+    //     if (distance < 0) {
+    //       clearInterval(countdown);
+    //       this.timerTarget.textContent = "EXPIRED";
+    //     } else {
+    //       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //       this.timerTarget.textContent = `${hours}h ${minutes}m ${seconds}s`;
+    //     }
+    //   };
+
+    // updateTimer();
+    // const countdown = setInterval(updateTimer, 1000);
+  }
+}
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -5,4 +5,5 @@ class Meeting < ApplicationRecord
   has_many :locations
 
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :start_time, presence: true
 end
